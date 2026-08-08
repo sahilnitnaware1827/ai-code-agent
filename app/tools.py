126211a1d5a96d519c2df29e96fafa124e0a226c
Sahil Nitnaware <sahilnitnaware71@gmail.com>
2026-08-08 11:56:55 +0530

    Add file reading tool

--- app/tools.py
+++ app/tools.py
@@ -15,3 +15,24 @@ def list_directory(path: str) -> list[str]:
 
     return [item.name for item in directory.iterdir()]
 
+
+
+def read_file(path: str) -> str:
+    """
+        Read and return content of a text file 
+    """
+
+    file = Path(path)
+
+    if not file.exists:
+        return "Error: File does not Exist"
+
+    if not file.is_file():
+        return "Error: Path is not a file"
+
+    try:
+        return file.read_text(encoding="utf-8")
+
+    except UnicodeDecodeError:
+        return "Error: file is not a utf-8 text file"
+    
--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,8 +1,12 @@
 # test tool.py file 
 
 from app.tools import list_directory
+from app.tools import read_file
 
 result = list_directory("workspace/sample_project")
 
-print(result)
+print("\n Files", result)
 
+
+print("\n\n main.py >>> ")
+print(read_file("workspace/sample_project/main.py"))
